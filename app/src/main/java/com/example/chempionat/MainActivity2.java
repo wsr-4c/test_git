7283f2947e8481395e1377423d0ff2e54c9ecb7a
WSR_4 <4.wsr@mail.ru>
2024-03-25 10:47:18 +0800

    testing5

--- app/src/main/java/com/example/chempionat/MainActivity2.java
+++ app/src/main/java/com/example/chempionat/MainActivity2.java
@@ -1,6 +1,8 @@
 package com.example.chempionat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,9 @@ public class MainActivity2 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void main3(View v){
+        Intent intent = new Intent(this, MainActivity3.class);
+        startActivity(intent);
     }
 }
